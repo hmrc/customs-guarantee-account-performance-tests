08ff2878fc650faeb472ca4bed4966bac1b59cc5
sameerhmrc <106383455+sameerhmrc@users.noreply.github.com>
2023-07-28 08:10:28 +0100

    MMCA-4155 : Update performance tests for repos

--- src/test/scala/uk/gov/hmrc/perftests/guaranteeaccount/AuthRequests.scala
+++ src/test/scala/uk/gov/hmrc/perftests/guaranteeaccount/AuthRequests.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 HM Revenue & Customs
+ * Copyright 2023 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -38,7 +38,8 @@ trait AuthRequests {
     "enrolment[0].taxIdentifier[0].value" -> "${eoriNumber}"
   )
 
-  setup("login", "Login") withRequests(
+  val loginSetup = setup("login", "Login")
+  loginSetup.withRequests(
     getPage("Auth Page", authUrl),
     postPage("Post Auth Page", postToken = false, authUrl, s"$baseUrl/customs/guarantee-account", loginStubPayload)
   )
