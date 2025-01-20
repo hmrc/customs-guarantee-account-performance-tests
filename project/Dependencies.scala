dc64aaf698c31f4a893ea4fd73d5eabbf4f0d978
Adam Cooper <adam.cooper2@digital.hmrc.gov.uk>
2025-01-20 11:48:21 +0000

    MMCA-5102-remove gatling

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,9 +1,6 @@
 import sbt._
 
 object Dependencies {
-
-  private val gatlingVersion = "3.6.1"
-
   val test = Seq(
     "com.typesafe"          % "config"                    % "1.4.2"        % Test,
     "uk.gov.hmrc"          %% "performance-test-runner"   % "6.1.0"        % Test
